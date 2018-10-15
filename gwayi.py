6311648ca06d27737d086da6abcb162a96c8e4cc
McGiant <tapiwakonga@gmail.com>
2018-10-15 14:11:59 +0200

    Isolating dmns

--- gwayi.py
+++ gwayi.py
@@ -1,10 +1,17 @@
 import os
 # Open the file with write permit
 f = open('/home/lawrence/Documents/gwayi.txt')
-
+rem_domain = []
 for line in f.readlines():
     x = f.readline()
     y = os.system('dig MX {}'.format(line))
+    zebb = os.popen('dig MX {}'.format(line)).read()
+    if 'yoafrica' not in zebb:
+        rem_domain.append(line)
+
     print(line)
+    print(rem_domain)
+
+
 
 f.close()
